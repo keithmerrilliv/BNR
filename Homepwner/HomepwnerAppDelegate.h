8676cc93def9de56a810947910c6f1b9bdc00824
Keith Merrill <keithmerrilliv@yahoo.ca>
2013-07-02 09:17:35 -0700

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Keith Merrill on 7/2/13.
+//  Copyright (c) 2013 Keith Merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Keith Merrill on 7/2/13.
+//  Copyright (c) 2013 Keith Merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
